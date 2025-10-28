624e3c4b57896f2ca030fbdf152472b2f049bbe1
salih <salih1504200473@mail.ru>
2025-10-28 23:16:44 +0400

    болванки тестов намечены

---
+++ src/test/java/tests/apitests/DataSelectTest.java
@@ -0,0 +1,52 @@
+package tests.apitests;
+
+public class DataSelectTest extends BasicTest {
+
+    void get_list_posts_positive() {
+
+    }
+
+    void get_post_by_id_positive() {
+
+    }
+
+    void get_non_exists_post_by_id_positive() {
+
+    }
+
+    void get_list_categories_positive() {
+
+    }
+
+    void get_category_by_id_positive() {
+
+    }
+
+    void get_list_comments_positive() {
+
+    }
+
+    void get_comment_by_id_positive() {
+
+    }
+
+    void get_posts_by_category_positive() {
+
+    }
+
+    void get_posts_by_author_positive() {
+
+    }
+
+    void get_posts_with_pagination_positive() {
+
+    }
+
+    void get_posts_with_filter_by_status() {
+
+    }
+
+    void get_posts_with_non_auth_user() {
+
+    }
+}
